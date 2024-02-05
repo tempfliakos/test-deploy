d50be44f4bb41df0c4acf7f7e4181b0821536717
Akos Tempfli <tempfli.akos@webtown.hu>
2024-02-05 15:41:37 +0100

    export module

---
+++ src/components/Test.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export function Test() {
+  return <h1>ASD</h1>
+}
---
+++ src/components/Test2.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+
+export default class Test2 extends Component {
+
+  render() {
+    return <h1>ASD</h1>
+  }
+}
